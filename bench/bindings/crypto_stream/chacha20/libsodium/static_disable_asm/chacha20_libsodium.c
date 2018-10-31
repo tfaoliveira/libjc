c52ada5e787bbf9c7a914b96a1256596b4e62596
Tiago Oliveira <tfaoliveira@gmail.com>
2018-10-31 16:18:59 +0000

    benchmark framework : first step

---
+++ bench/bindings/crypto_stream/chacha20/libsodium/static_disable_asm/chacha20_libsodium.c
@@ -0,0 +1,36 @@
+#include "crypto_stream.h"
+#include <sodium/crypto_stream_chacha20.h>
+#include <string.h>
+
+int crypto_stream(
+  unsigned char *out,
+  unsigned long long outlen,
+  const unsigned char *n,
+  const unsigned char *k
+)
+{
+  unsigned char nonce[12];
+  supercop_static_disable_asm_sodium_init();
+  memset(out, 0, outlen);
+  memset(nonce, 0, 4);
+  memcpy(nonce+4, n, 8);
+  supercop_static_disable_asm_crypto_stream_chacha20_ietf(out, outlen, nonce, k);
+	return 0;
+}
+
+int crypto_stream_xor(
+  unsigned char *out,
+  const unsigned char *in,
+  unsigned long long inlen,
+  const unsigned char *n,
+  const unsigned char *k
+)
+{
+  unsigned char nonce[12];
+  supercop_static_disable_asm_sodium_init();
+  memset(nonce, 0, 4);
+  memcpy(nonce+4, n, 8);
+  supercop_static_disable_asm_crypto_stream_chacha20_ietf_xor(out, in, inlen, nonce, k);
+	return 0;
+}
+
---
+++ bench/bindings/crypto_stream/chacha20/openssl/static/chacha20_supercop.c
@@ -0,0 +1,32 @@
+#include "crypto_stream.h"
+#include <string.h>
+
+int crypto_stream(
+  unsigned char *out,
+  unsigned long long outlen,
+  const unsigned char *n,
+  const unsigned char *k
+)
+{
+  unsigned char nonce[16];
+  memset(out, 0, outlen);
+  memset(nonce, 0, 8);
+  memcpy(nonce+8, n, 8);
+  supercop_static_ChaCha20_ctr32(out, out, outlen, k, nonce);
+	return 0;
+}
+
+int crypto_stream_xor(
+  unsigned char *out,
+  const unsigned char *in,
+  unsigned long long inlen,
+  const unsigned char *n,
+  const unsigned char *k
+)
+{
+  unsigned char nonce[16];
+  memset(nonce, 0, 8);
+  memcpy(nonce+8, n, 8);
+  supercop_static_ChaCha20_ctr32(out, in, inlen, k, nonce);
+  return 0;
+}
