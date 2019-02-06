b6621b5dea1c1699306cb8b477050bffa6186d39
Tiago Oliveira <tfaoliveira@gmail.com>
2019-02-06 18:24:10 +0000

    add prev. impl of x25519

---
+++ bench/bindings/crypto_scalarmult/curve25519/jazz/curve25519_jazz.c
@@ -0,0 +1,36 @@
+#include <string.h>
+#include "crypto_scalarmult.h"
+#include "impl.h"
+
+extern int curve25519_impl(
+  unsigned char *q,
+  const unsigned char *n,
+  const unsigned char *p
+);
+
+static const unsigned char basepoint[32] = {9};
+
+int crypto_scalarmult(
+  unsigned char *q,
+  const unsigned char *n,
+  const unsigned char *p
+)
+{
+  int r;
+  unsigned char n_[32], p_[32];
+  memcpy(n_, n, 32);
+  memcpy(p_, p, 32);
+  r = curve25519_impl(q,n_,p_);
+  return 0;
+}
+
+int crypto_scalarmult_base(
+  unsigned char *q,
+  const unsigned char *n
+)
+{
+  int r;
+  r = crypto_scalarmult(q,n,basepoint);
+  return 0;
+}
+
