3f511623f9cecf0432b020c1049f7520b30eebcd
Tiago Oliveira <tfaoliveira@gmail.com>
2019-04-12 04:55:50 +0100

    openssl keccak bench

---
+++ bench/patch/20190110/crypto_hash/measure.c
@@ -0,0 +1,89 @@
+#include <stdlib.h>
+#include "kernelrandombytes.h"
+#include "cpucycles.h"
+#include "crypto_hash.h"
+#include "measure.h"
+
+const char *primitiveimplementation = crypto_hash_IMPLEMENTATION;
+const char *implementationversion = crypto_hash_VERSION;
+const char *sizenames[] = { "outputbytes", 0 };
+const long long sizes[] = { crypto_hash_BYTES };
+
+#define MAXTEST_BYTES 16384
+#ifdef SUPERCOP
+#define MGAP 8192
+#else
+#define MGAP 8
+#endif
+
+static unsigned char *h;
+static unsigned char *m;
+
+void preallocate(void)
+{
+}
+
+void allocate(void)
+{
+  h = alignedcalloc(crypto_hash_BYTES);
+  m = alignedcalloc(MAXTEST_BYTES);
+}
+
+#define TIMINGS 127
+static long long cycles[TIMINGS + 1];
+
+int update_increment_setup1(int mlen)
+{
+  if(mlen < 64)
+    return 1;
+  if(mlen < 128)
+    return 2;
+  if(mlen < 256)
+    return 4;
+  if(mlen < 512)
+    return 8;
+  if(mlen < 1024)
+    return 16;
+  if(mlen < 2048)
+    return 32;
+  if(mlen < 4096)
+    return 64;
+  if(mlen < 8192)
+    return 128;
+  if(mlen < 16384)
+    return 256;
+  return 512;
+}
+
+int update_increment(int mlen)
+{
+  return update_increment_setup1(mlen);
+}
+
+static void printcycles(long long mlen)
+{
+  int i;
+  for (i = 0;i < TIMINGS;++i) cycles[i] = cycles[i + 1] - cycles[i];
+  printentry(mlen,"cycles",cycles,TIMINGS);
+}
+
+void measure(void)
+{
+  int i;
+  int loop;
+  int mlen;
+  int inc = 1;
+
+  for (loop = 0;loop < LOOPS;++loop) {
+    for (mlen = 0;mlen <= MAXTEST_BYTES;mlen += inc) {
+      kernelrandombytes(m,mlen);
+      for (i = 0;i <= TIMINGS;++i) {
+        cycles[i] = cpucycles();
+        crypto_hash(h,m,mlen);
+      }
+      printcycles(mlen);
+
+      inc = update_increment(mlen);
+    }
+  }
+}
