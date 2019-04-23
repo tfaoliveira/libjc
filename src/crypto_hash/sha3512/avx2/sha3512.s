ff0c1feb759445d4a3c503ba88ba9c0698f6dbf9
Tiago Oliveira <tfaoliveira@gmail.com>
2019-04-23 11:20:11 +0100

    sha3512 : avx2 : fix add full block

--- src/crypto_hash/sha3512/avx2/sha3512.s
+++ src/crypto_hash/sha3512/avx2/sha3512.s
@@ -7,13 +7,13 @@ sha3512_avx2_jazz:
 	pushq	%rbp
 	pushq	%rbx
 	subq	$224, %rsp
-	vpbroadcastq	g_zero(%rip), %ymm5
-	vmovdqu	%ymm5, %ymm6
-	vmovdqu	%ymm5, %ymm8
-	vmovdqu	%ymm5, %ymm9
-	vmovdqu	%ymm5, %ymm10
-	vmovdqu	%ymm5, %ymm0
-	vmovdqu	%ymm5, %ymm11
+	vpbroadcastq	g_zero(%rip), %ymm6
+	vmovdqu	%ymm6, %ymm7
+	vmovdqu	%ymm6, %ymm8
+	vmovdqu	%ymm6, %ymm9
+	vmovdqu	%ymm6, %ymm10
+	vmovdqu	%ymm6, %ymm0
+	vmovdqu	%ymm6, %ymm11
 	movq	(%rcx), %rax
 	movq	8(%rcx), %r8
 	movq	16(%rcx), %r9
@@ -22,17 +22,18 @@ sha3512_avx2_jazz:
 Lsha3512_avx2_jazz$11:
 	vpbroadcastq	s_zero(%rip), %ymm1
 	vpbroadcastq	(%rsi), %ymm2
-	vmovdqu	8(%rsi), %ymm3
-	vmovdqu	48(%rsi), %ymm4
-	vpxor	%ymm2, %ymm5, %ymm5
-	vpxor	%ymm3, %ymm6, %ymm6
+	vpbroadcastq	40(%rsi), %ymm3
+	vmovdqu	8(%rsi), %ymm4
+	vmovdqu	48(%rsi), %ymm5
+	vpxor	%ymm2, %ymm6, %ymm6
+	vpxor	%ymm4, %ymm7, %ymm7
 	vpblendd	$-49, %ymm1, %ymm3, %ymm2
-	vpblendd	$-13, %ymm1, %ymm4, %ymm3
-	vpblendd	$-49, %ymm1, %ymm4, %ymm7
-	vpblendd	$-4, %ymm1, %ymm4, %ymm1
+	vpblendd	$-13, %ymm1, %ymm5, %ymm3
+	vpblendd	$-49, %ymm1, %ymm5, %ymm4
+	vpblendd	$-4, %ymm1, %ymm5, %ymm1
 	vpxor	%ymm2, %ymm8, %ymm8
 	vpxor	%ymm3, %ymm9, %ymm9
-	vpxor	%ymm7, %ymm10, %ymm10
+	vpxor	%ymm4, %ymm10, %ymm10
 	vpxor	%ymm1, %ymm11, %ymm11
 	leaq	72(%rsi), %rsi
 	leaq	-72(%rdx), %rdx
@@ -45,25 +46,25 @@ Lsha3512_avx2_jazz$12:
 	vpshufd	$78, %ymm8, %ymm1
 	vpxor	%ymm9, %ymm0, %ymm2
 	vpxor	%ymm11, %ymm10, %ymm3
-	vpxor	%ymm6, %ymm2, %ymm2
+	vpxor	%ymm7, %ymm2, %ymm2
 	vpxor	%ymm3, %ymm2, %ymm2
 	vpermq	$-109, %ymm2, %ymm3
 	vpxor	%ymm8, %ymm1, %ymm1
 	vpermq	$78, %ymm1, %ymm4
-	vpsrlq	$63, %ymm2, %ymm7
+	vpsrlq	$63, %ymm2, %ymm5
 	vpaddq	%ymm2, %ymm2, %ymm2
-	vpor	%ymm2, %ymm7, %ymm2
-	vpermq	$57, %ymm2, %ymm7
+	vpor	%ymm2, %ymm5, %ymm2
+	vpermq	$57, %ymm2, %ymm5
 	vpxor	%ymm3, %ymm2, %ymm2
 	vpermq	$0, %ymm2, %ymm2
-	vpxor	%ymm5, %ymm1, %ymm1
+	vpxor	%ymm6, %ymm1, %ymm1
 	vpxor	%ymm4, %ymm1, %ymm1
 	vpsrlq	$63, %ymm1, %ymm4
 	vpaddq	%ymm1, %ymm1, %ymm12
 	vpor	%ymm4, %ymm12, %ymm4
 	vpxor	%ymm2, %ymm8, %ymm8
-	vpxor	%ymm2, %ymm5, %ymm2
-	vpblendd	$-64, %ymm4, %ymm7, %ymm4
+	vpxor	%ymm2, %ymm6, %ymm2
+	vpblendd	$-64, %ymm4, %ymm5, %ymm4
 	vpblendd	$3, %ymm1, %ymm3, %ymm1
 	vpxor	%ymm1, %ymm4, %ymm1
 	vpsllvq	-96(%r10), %ymm8, %ymm3
@@ -74,38 +75,38 @@ Lsha3512_avx2_jazz$12:
 	vpsrlvq	-32(%r11), %ymm4, %ymm4
 	vpor	%ymm5, %ymm4, %ymm4
 	vpxor	%ymm1, %ymm10, %ymm5
-	vpsllvq	(%r10), %ymm5, %ymm7
+	vpsllvq	(%r10), %ymm5, %ymm6
 	vpsrlvq	(%r11), %ymm5, %ymm5
-	vpor	%ymm7, %ymm5, %ymm5
+	vpor	%ymm6, %ymm5, %ymm5
 	vpxor	%ymm1, %ymm0, %ymm0
-	vpsllvq	32(%r10), %ymm0, %ymm7
+	vpsllvq	32(%r10), %ymm0, %ymm6
 	vpsrlvq	32(%r11), %ymm0, %ymm0
-	vpor	%ymm7, %ymm0, %ymm0
-	vpxor	%ymm1, %ymm11, %ymm7
+	vpor	%ymm6, %ymm0, %ymm0
+	vpxor	%ymm1, %ymm11, %ymm6
 	vpermq	$-115, %ymm3, %ymm3
 	vpermq	$-115, %ymm4, %ymm4
-	vpsllvq	64(%r10), %ymm7, %ymm8
-	vpsrlvq	64(%r11), %ymm7, %ymm7
-	vpor	%ymm8, %ymm7, %ymm7
-	vpxor	%ymm1, %ymm6, %ymm1
+	vpsllvq	64(%r10), %ymm6, %ymm8
+	vpsrlvq	64(%r11), %ymm6, %ymm6
+	vpor	%ymm8, %ymm6, %ymm6
+	vpxor	%ymm1, %ymm7, %ymm1
 	vpermq	$27, %ymm5, %ymm5
-	vpermq	$114, %ymm0, %ymm6
+	vpermq	$114, %ymm0, %ymm7
 	vpsllvq	-64(%r10), %ymm1, %ymm0
 	vpsrlvq	-64(%r11), %ymm1, %ymm1
 	vpor	%ymm0, %ymm1, %ymm1
-	vpsrldq	$8, %ymm7, %ymm0
-	vpandn	%ymm0, %ymm7, %ymm0
-	vpblendd	$12, %ymm6, %ymm1, %ymm8
+	vpsrldq	$8, %ymm6, %ymm0
+	vpandn	%ymm0, %ymm6, %ymm0
+	vpblendd	$12, %ymm7, %ymm1, %ymm8
 	vpblendd	$12, %ymm1, %ymm4, %ymm9
 	vpblendd	$12, %ymm4, %ymm3, %ymm10
 	vpblendd	$12, %ymm3, %ymm1, %ymm11
 	vpblendd	$48, %ymm4, %ymm8, %ymm8
 	vpblendd	$48, %ymm5, %ymm9, %ymm9
 	vpblendd	$48, %ymm1, %ymm10, %ymm10
-	vpblendd	$48, %ymm6, %ymm11, %ymm11
+	vpblendd	$48, %ymm7, %ymm11, %ymm11
 	vpblendd	$-64, %ymm5, %ymm8, %ymm8
-	vpblendd	$-64, %ymm6, %ymm9, %ymm9
-	vpblendd	$-64, %ymm6, %ymm10, %ymm10
+	vpblendd	$-64, %ymm7, %ymm9, %ymm9
+	vpblendd	$-64, %ymm7, %ymm10, %ymm10
 	vpblendd	$-64, %ymm4, %ymm11, %ymm11
 	vpandn	%ymm9, %ymm8, %ymm8
 	vpandn	%ymm11, %ymm10, %ymm9
@@ -118,15 +119,15 @@ Lsha3512_avx2_jazz$12:
 	vpblendd	$-64, %ymm4, %ymm8, %ymm8
 	vpblendd	$-64, %ymm1, %ymm10, %ymm9
 	vpandn	%ymm9, %ymm8, %ymm8
-	vpxor	%ymm6, %ymm8, %ymm10
-	vpermq	$30, %ymm7, %ymm8
+	vpxor	%ymm7, %ymm8, %ymm10
+	vpermq	$30, %ymm6, %ymm8
 	vpblendd	$48, %ymm2, %ymm8, %ymm8
-	vpermq	$57, %ymm7, %ymm9
+	vpermq	$57, %ymm6, %ymm9
 	vpblendd	$-64, %ymm2, %ymm9, %ymm9
 	vpandn	%ymm8, %ymm9, %ymm13
 	vpblendd	$12, %ymm5, %ymm4, %ymm8
-	vpblendd	$12, %ymm4, %ymm6, %ymm9
-	vpblendd	$48, %ymm6, %ymm8, %ymm8
+	vpblendd	$12, %ymm4, %ymm7, %ymm9
+	vpblendd	$48, %ymm7, %ymm8, %ymm8
 	vpblendd	$48, %ymm3, %ymm9, %ymm9
 	vpblendd	$-64, %ymm3, %ymm8, %ymm8
 	vpblendd	$-64, %ymm5, %ymm9, %ymm9
@@ -136,17 +137,17 @@ Lsha3512_avx2_jazz$12:
 	vpermq	$27, %ymm12, %ymm9
 	vpermq	$-115, %ymm11, %ymm0
 	vpermq	$114, %ymm10, %ymm11
-	vpblendd	$12, %ymm3, %ymm6, %ymm10
-	vpblendd	$12, %ymm6, %ymm5, %ymm6
+	vpblendd	$12, %ymm3, %ymm7, %ymm10
+	vpblendd	$12, %ymm7, %ymm5, %ymm7
 	vpblendd	$48, %ymm5, %ymm10, %ymm5
-	vpblendd	$48, %ymm1, %ymm6, %ymm6
+	vpblendd	$48, %ymm1, %ymm7, %ymm7
 	vpblendd	$-64, %ymm1, %ymm5, %ymm1
-	vpblendd	$-64, %ymm3, %ymm6, %ymm3
+	vpblendd	$-64, %ymm3, %ymm7, %ymm3
 	vpandn	%ymm3, %ymm1, %ymm1
 	vpxor	%ymm14, %ymm2, %ymm2
-	vpxor	%ymm7, %ymm13, %ymm6
+	vpxor	%ymm6, %ymm13, %ymm7
 	vpxor	%ymm4, %ymm1, %ymm10
-	vpxor	(%rbp), %ymm2, %ymm5
+	vpxor	(%rbp), %ymm2, %ymm6
 	leaq	32(%rbp), %rbp
 	decl	%ebx
 	jne 	Lsha3512_avx2_jazz$12
@@ -195,8 +196,8 @@ Lsha3512_avx2_jazz$6:
 	movq	%rdx, 8(%rsp)
 	movq	%rdx, 16(%rsp)
 	movq	%rdx, 24(%rsp)
-	vpxor	(%rsp), %ymm5, %ymm1
-	vpxor	32(%rsp), %ymm6, %ymm2
+	vpxor	(%rsp), %ymm6, %ymm1
+	vpxor	32(%rsp), %ymm7, %ymm2
 	vpxor	64(%rsp), %ymm8, %ymm3
 	vpxor	96(%rsp), %ymm9, %ymm4
 	vpxor	128(%rsp), %ymm10, %ymm5
