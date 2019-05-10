c5b555d3d9966f0a4c044bf25e8a84bd512b3cfa
Tiago Oliveira <tfaoliveira@gmail.com>
2019-05-11 00:37:49 +0100

    keccak1600 avx2 src closed

--- src/crypto_hash/sha3512/avx2/sha3512.s
+++ src/crypto_hash/sha3512/avx2/sha3512.s
@@ -1,12 +1,22 @@
 	.text
 	.p2align	5
-	.globl	_sha3512_avx2_jazz
-	.globl	sha3512_avx2_jazz
-_sha3512_avx2_jazz:
-sha3512_avx2_jazz:
+	.globl	_keccak_1600
+	.globl	keccak_1600
+_keccak_1600:
+keccak_1600:
 	pushq	%rbp
 	pushq	%rbx
+	pushq	%r12
+	pushq	%r13
+	pushq	%r14
 	subq	$224, %rsp
+	movb	(%r8), %al
+	movq	8(%r8), %r8
+	shrq	$3, %r8
+	movq	(%r9), %r10
+	movq	8(%r9), %r11
+	movq	16(%r9), %rbp
+	movq	24(%r9), %r9
 	vpbroadcastq	g_zero(%rip), %ymm6
 	vmovdqu	%ymm6, %ymm7
 	vmovdqu	%ymm6, %ymm8
@@ -14,17 +24,13 @@ sha3512_avx2_jazz:
 	vmovdqu	%ymm6, %ymm10
 	vmovdqu	%ymm6, %ymm0
 	vmovdqu	%ymm6, %ymm11
-	movq	(%rcx), %rax
-	movq	8(%rcx), %r8
-	movq	16(%rcx), %r9
-	movq	24(%rcx), %rcx
-	jmp 	Lsha3512_avx2_jazz$10
-Lsha3512_avx2_jazz$11:
+	jmp 	Lkeccak_1600$15
+Lkeccak_1600$16:
 	vpbroadcastq	s_zero(%rip), %ymm1
-	vpbroadcastq	(%rsi), %ymm2
-	vpbroadcastq	40(%rsi), %ymm3
-	vmovdqu	8(%rsi), %ymm4
-	vmovdqu	48(%rsi), %ymm5
+	vpbroadcastq	(%rdx), %ymm2
+	vpbroadcastq	40(%rdx), %ymm3
+	vmovdqu	8(%rdx), %ymm4
+	vmovdqu	48(%rdx), %ymm5
 	vpxor	%ymm2, %ymm6, %ymm6
 	vpxor	%ymm4, %ymm7, %ymm7
 	vpblendd	$-49, %ymm1, %ymm3, %ymm2
@@ -35,14 +41,14 @@ Lsha3512_avx2_jazz$11:
 	vpxor	%ymm3, %ymm9, %ymm9
 	vpxor	%ymm4, %ymm10, %ymm10
 	vpxor	%ymm1, %ymm11, %ymm11
-	leaq	72(%rsi), %rsi
-	leaq	-72(%rdx), %rdx
-	leaq	96(%rax), %r10
-	leaq	96(%r8), %r11
-	movq	%r9, %rbp
-	movl	$24, %ebx
+	leaq	(%rdx,%r8), %rdx
+	subq	%r8, %rcx
+	leaq	96(%r10), %rbx
+	leaq	96(%r11), %r12
+	movq	%rbp, %r13
+	movl	$24, %r14d
 	.p2align	5
-Lsha3512_avx2_jazz$12:
+Lkeccak_1600$17:
 	vpshufd	$78, %ymm8, %ymm1
 	vpxor	%ymm9, %ymm0, %ymm2
 	vpxor	%ymm11, %ymm10, %ymm3
@@ -67,32 +73,32 @@ Lsha3512_avx2_jazz$12:
 	vpblendd	$-64, %ymm4, %ymm5, %ymm4
 	vpblendd	$3, %ymm1, %ymm3, %ymm1
 	vpxor	%ymm1, %ymm4, %ymm1
-	vpsllvq	-96(%r10), %ymm8, %ymm3
-	vpsrlvq	-96(%r11), %ymm8, %ymm4
+	vpsllvq	-96(%rbx), %ymm8, %ymm3
+	vpsrlvq	-96(%r12), %ymm8, %ymm4
 	vpor	%ymm3, %ymm4, %ymm3
 	vpxor	%ymm1, %ymm9, %ymm4
-	vpsllvq	-32(%r10), %ymm4, %ymm5
-	vpsrlvq	-32(%r11), %ymm4, %ymm4
+	vpsllvq	-32(%rbx), %ymm4, %ymm5
+	vpsrlvq	-32(%r12), %ymm4, %ymm4
 	vpor	%ymm5, %ymm4, %ymm4
 	vpxor	%ymm1, %ymm10, %ymm5
-	vpsllvq	(%r10), %ymm5, %ymm6
-	vpsrlvq	(%r11), %ymm5, %ymm5
+	vpsllvq	(%rbx), %ymm5, %ymm6
+	vpsrlvq	(%r12), %ymm5, %ymm5
 	vpor	%ymm6, %ymm5, %ymm5
 	vpxor	%ymm1, %ymm0, %ymm0
-	vpsllvq	32(%r10), %ymm0, %ymm6
-	vpsrlvq	32(%r11), %ymm0, %ymm0
+	vpsllvq	32(%rbx), %ymm0, %ymm6
+	vpsrlvq	32(%r12), %ymm0, %ymm0
 	vpor	%ymm6, %ymm0, %ymm0
 	vpxor	%ymm1, %ymm11, %ymm6
 	vpermq	$-115, %ymm3, %ymm3
 	vpermq	$-115, %ymm4, %ymm4
-	vpsllvq	64(%r10), %ymm6, %ymm8
-	vpsrlvq	64(%r11), %ymm6, %ymm6
+	vpsllvq	64(%rbx), %ymm6, %ymm8
+	vpsrlvq	64(%r12), %ymm6, %ymm6
 	vpor	%ymm8, %ymm6, %ymm6
 	vpxor	%ymm1, %ymm7, %ymm1
 	vpermq	$27, %ymm5, %ymm5
 	vpermq	$114, %ymm0, %ymm7
-	vpsllvq	-64(%r10), %ymm1, %ymm0
-	vpsrlvq	-64(%r11), %ymm1, %ymm1
+	vpsllvq	-64(%rbx), %ymm1, %ymm0
+	vpsrlvq	-64(%r12), %ymm1, %ymm1
 	vpor	%ymm0, %ymm1, %ymm1
 	vpsrldq	$8, %ymm6, %ymm0
 	vpandn	%ymm0, %ymm6, %ymm0
@@ -147,13 +153,13 @@ Lsha3512_avx2_jazz$12:
 	vpxor	%ymm14, %ymm2, %ymm2
 	vpxor	%ymm6, %ymm13, %ymm7
 	vpxor	%ymm4, %ymm1, %ymm10
-	vpxor	(%rbp), %ymm2, %ymm6
-	leaq	32(%rbp), %rbp
-	decl	%ebx
-	jne 	Lsha3512_avx2_jazz$12
-Lsha3512_avx2_jazz$10:
-	cmpq	$72, %rdx
-	jnb 	Lsha3512_avx2_jazz$11
+	vpxor	(%r13), %ymm2, %ymm6
+	leaq	32(%r13), %r13
+	decl	%r14d
+	jne 	Lkeccak_1600$17
+Lkeccak_1600$15:
+	cmpq	%r8, %rcx
+	jnb 	Lkeccak_1600$16
 	vpbroadcastq	g_zero(%rip), %ymm1
 	vmovdqu	%ymm1, (%rsp)
 	vmovdqu	%ymm1, 32(%rsp)
@@ -162,40 +168,45 @@ Lsha3512_avx2_jazz$10:
 	vmovdqu	%ymm1, 128(%rsp)
 	vmovdqu	%ymm1, 160(%rsp)
 	vmovdqu	%ymm1, 192(%rsp)
-	movq	%rdx, %r10
-	shrq	$3, %r10
-	movq	$0, %r11
-	jmp 	Lsha3512_avx2_jazz$8
-Lsha3512_avx2_jazz$9:
-	movq	(%rsi,%r11,8), %rbp
-	movq	(%rcx,%r11,8), %rbx
-	movq	%rbp, (%rsp,%rbx,8)
-	leaq	1(%r11), %r11
-Lsha3512_avx2_jazz$8:
-	cmpq	%r10, %r11
-	jb  	Lsha3512_avx2_jazz$9
-	movq	(%rcx,%r11,8), %r10
-	shlq	$3, %r11
-	shlq	$3, %r10
-	jmp 	Lsha3512_avx2_jazz$6
-Lsha3512_avx2_jazz$7:
-	movb	(%rsi,%r11), %bpl
-	movb	%bpl, (%rsp,%r10)
-	leaq	1(%r11), %r11
-	leaq	1(%r10), %r10
-Lsha3512_avx2_jazz$6:
-	cmpq	%rdx, %r11
-	jb  	Lsha3512_avx2_jazz$7
-	movb	$6, (%rsp,%r10)
-	movq	$8, %rdx
-	movq	(%rcx,%rdx,8), %rdx
-	shlq	$3, %rdx
-	leaq	7(%rdx), %rdx
-	xorb	$-128, (%rsp,%rdx)
-	movq	(%rsp), %rdx
-	movq	%rdx, 8(%rsp)
-	movq	%rdx, 16(%rsp)
-	movq	%rdx, 24(%rsp)
+	movq	%rcx, %rbx
+	shrq	$3, %rbx
+	movq	$0, %r12
+	jmp 	Lkeccak_1600$13
+Lkeccak_1600$14:
+	movq	(%rdx,%r12,8), %r13
+	movq	(%r9,%r12,8), %r14
+	movq	%r13, (%rsp,%r14,8)
+	leaq	1(%r12), %r12
+Lkeccak_1600$13:
+	cmpq	%rbx, %r12
+	jb  	Lkeccak_1600$14
+	movq	(%r9,%r12,8), %rbx
+	shlq	$3, %r12
+	shlq	$3, %rbx
+	jmp 	Lkeccak_1600$11
+Lkeccak_1600$12:
+	movb	(%rdx,%r12), %r13b
+	movb	%r13b, (%rsp,%rbx)
+	leaq	1(%r12), %r12
+	leaq	1(%rbx), %rbx
+Lkeccak_1600$11:
+	cmpq	%rcx, %r12
+	jb  	Lkeccak_1600$12
+	movb	%al, (%rsp,%rbx)
+	movq	%r8, %rax
+	leaq	-1(%rax), %rax
+	shrq	$3, %rax
+	movq	(%r9,%rax,8), %rax
+	shlq	$3, %rax
+	movq	%r8, %rcx
+	leaq	-1(%rcx), %rcx
+	andq	$7, %rcx
+	leaq	(%rax,%rcx), %rax
+	xorb	$-128, (%rsp,%rax)
+	movq	(%rsp), %rax
+	movq	%rax, 8(%rsp)
+	movq	%rax, 16(%rsp)
+	movq	%rax, 24(%rsp)
 	vpxor	(%rsp), %ymm6, %ymm1
 	vpxor	32(%rsp), %ymm7, %ymm2
 	vpxor	64(%rsp), %ymm8, %ymm3
@@ -203,11 +214,14 @@ Lsha3512_avx2_jazz$6:
 	vpxor	128(%rsp), %ymm10, %ymm5
 	vpxor	160(%rsp), %ymm0, %ymm0
 	vpxor	192(%rsp), %ymm11, %ymm6
-	leaq	96(%rax), %rax
-	leaq	96(%r8), %rdx
-	movl	$24, %esi
+	jmp 	Lkeccak_1600$6
+Lkeccak_1600$7:
+	leaq	96(%r10), %rax
+	leaq	96(%r11), %rcx
+	movq	%rbp, %rdx
+	movl	$24, %ebx
 	.p2align	5
-Lsha3512_avx2_jazz$5:
+Lkeccak_1600$10:
 	vpshufd	$78, %ymm3, %ymm7
 	vpxor	%ymm4, %ymm0, %ymm8
 	vpxor	%ymm6, %ymm5, %ymm9
@@ -233,31 +247,31 @@ Lsha3512_avx2_jazz$5:
 	vpblendd	$3, %ymm7, %ymm9, %ymm7
 	vpxor	%ymm7, %ymm8, %ymm7
 	vpsllvq	-96(%rax), %ymm3, %ymm8
-	vpsrlvq	-96(%rdx), %ymm3, %ymm3
+	vpsrlvq	-96(%rcx), %ymm3, %ymm3
 	vpor	%ymm8, %ymm3, %ymm3
 	vpxor	%ymm7, %ymm4, %ymm4
 	vpsllvq	-32(%rax), %ymm4, %ymm8
-	vpsrlvq	-32(%rdx), %ymm4, %ymm4
+	vpsrlvq	-32(%rcx), %ymm4, %ymm4
 	vpor	%ymm8, %ymm4, %ymm4
 	vpxor	%ymm7, %ymm5, %ymm5
 	vpsllvq	(%rax), %ymm5, %ymm8
-	vpsrlvq	(%rdx), %ymm5, %ymm5
+	vpsrlvq	(%rcx), %ymm5, %ymm5
 	vpor	%ymm8, %ymm5, %ymm5
 	vpxor	%ymm7, %ymm0, %ymm0
 	vpsllvq	32(%rax), %ymm0, %ymm8
-	vpsrlvq	32(%rdx), %ymm0, %ymm0
+	vpsrlvq	32(%rcx), %ymm0, %ymm0
 	vpor	%ymm8, %ymm0, %ymm0
 	vpxor	%ymm7, %ymm6, %ymm6
 	vpermq	$-115, %ymm3, %ymm8
 	vpermq	$-115, %ymm4, %ymm9
 	vpsllvq	64(%rax), %ymm6, %ymm3
-	vpsrlvq	64(%rdx), %ymm6, %ymm4
+	vpsrlvq	64(%rcx), %ymm6, %ymm4
 	vpor	%ymm3, %ymm4, %ymm10
 	vpxor	%ymm7, %ymm2, %ymm2
 	vpermq	$27, %ymm5, %ymm5
 	vpermq	$114, %ymm0, %ymm7
 	vpsllvq	-64(%rax), %ymm2, %ymm0
-	vpsrlvq	-64(%rdx), %ymm2, %ymm2
+	vpsrlvq	-64(%rcx), %ymm2, %ymm2
 	vpor	%ymm0, %ymm2, %ymm2
 	vpsrldq	$8, %ymm10, %ymm0
 	vpandn	%ymm0, %ymm10, %ymm0
@@ -312,11 +326,10 @@ Lsha3512_avx2_jazz$5:
 	vpxor	%ymm14, %ymm1, %ymm1
 	vpxor	%ymm10, %ymm13, %ymm2
 	vpxor	%ymm9, %ymm5, %ymm5
-	vpxor	(%r9), %ymm1, %ymm1
-	leaq	32(%r9), %r9
-	decl	%esi
-	jne 	Lsha3512_avx2_jazz$5
-	movq	$64, %rax
+	vpxor	(%rdx), %ymm1, %ymm1
+	leaq	32(%rdx), %rdx
+	decl	%ebx
+	jne 	Lkeccak_1600$10
 	vmovdqu	%ymm1, (%rsp)
 	vmovdqu	%ymm2, 32(%rsp)
 	vmovdqu	%ymm3, 64(%rsp)
@@ -324,31 +337,171 @@ Lsha3512_avx2_jazz$5:
 	vmovdqu	%ymm5, 128(%rsp)
 	vmovdqu	%ymm0, 160(%rsp)
 	vmovdqu	%ymm6, 192(%rsp)
-	movq	%rax, %rdx
-	shrq	$3, %rdx
-	movq	$0, %rsi
-	jmp 	Lsha3512_avx2_jazz$3
-Lsha3512_avx2_jazz$4:
-	movq	(%rcx,%rsi,8), %r8
-	movq	(%rsp,%r8,8), %r8
-	movq	%r8, (%rdi,%rsi,8)
-	leaq	1(%rsi), %rsi
-Lsha3512_avx2_jazz$3:
-	cmpq	%rdx, %rsi
-	jb  	Lsha3512_avx2_jazz$4
-	movq	(%rcx,%rsi,8), %rcx
-	shlq	$3, %rsi
-	shlq	$3, %rcx
-	jmp 	Lsha3512_avx2_jazz$1
-Lsha3512_avx2_jazz$2:
-	movb	(%rsp,%rcx), %dl
-	movb	%dl, (%rdi,%rsi)
-	leaq	1(%rsi), %rsi
+	movq	%r8, %rax
+	shrq	$3, %rax
+	movq	$0, %rcx
+	jmp 	Lkeccak_1600$8
+Lkeccak_1600$9:
+	movq	(%r9,%rcx,8), %rdx
+	movq	(%rsp,%rdx,8), %rdx
+	movq	%rdx, (%rdi,%rcx,8)
 	leaq	1(%rcx), %rcx
-Lsha3512_avx2_jazz$1:
-	cmpq	%rax, %rsi
-	jb  	Lsha3512_avx2_jazz$2
+Lkeccak_1600$8:
+	cmpq	%rax, %rcx
+	jb  	Lkeccak_1600$9
+	leaq	(%rdi,%r8), %rdi
+	subq	%r8, %rsi
+Lkeccak_1600$6:
+	cmpq	%r8, %rsi
+	jnbe	Lkeccak_1600$7
+	leaq	96(%r10), %rax
+	leaq	96(%r11), %rcx
+	movl	$24, %edx
+	.p2align	5
+Lkeccak_1600$5:
+	vpshufd	$78, %ymm3, %ymm7
+	vpxor	%ymm4, %ymm0, %ymm8
+	vpxor	%ymm6, %ymm5, %ymm9
+	vpxor	%ymm2, %ymm8, %ymm8
+	vpxor	%ymm9, %ymm8, %ymm8
+	vpermq	$-109, %ymm8, %ymm9
+	vpxor	%ymm3, %ymm7, %ymm7
+	vpermq	$78, %ymm7, %ymm10
+	vpsrlq	$63, %ymm8, %ymm11
+	vpaddq	%ymm8, %ymm8, %ymm8
+	vpor	%ymm8, %ymm11, %ymm8
+	vpermq	$57, %ymm8, %ymm11
+	vpxor	%ymm9, %ymm8, %ymm8
+	vpermq	$0, %ymm8, %ymm8
+	vpxor	%ymm1, %ymm7, %ymm7
+	vpxor	%ymm10, %ymm7, %ymm7
+	vpsrlq	$63, %ymm7, %ymm10
+	vpaddq	%ymm7, %ymm7, %ymm12
+	vpor	%ymm10, %ymm12, %ymm10
+	vpxor	%ymm8, %ymm3, %ymm3
+	vpxor	%ymm8, %ymm1, %ymm1
+	vpblendd	$-64, %ymm10, %ymm11, %ymm8
+	vpblendd	$3, %ymm7, %ymm9, %ymm7
+	vpxor	%ymm7, %ymm8, %ymm7
+	vpsllvq	-96(%rax), %ymm3, %ymm8
+	vpsrlvq	-96(%rcx), %ymm3, %ymm3
+	vpor	%ymm8, %ymm3, %ymm3
+	vpxor	%ymm7, %ymm4, %ymm4
+	vpsllvq	-32(%rax), %ymm4, %ymm8
+	vpsrlvq	-32(%rcx), %ymm4, %ymm4
+	vpor	%ymm8, %ymm4, %ymm4
+	vpxor	%ymm7, %ymm5, %ymm5
+	vpsllvq	(%rax), %ymm5, %ymm8
+	vpsrlvq	(%rcx), %ymm5, %ymm5
+	vpor	%ymm8, %ymm5, %ymm5
+	vpxor	%ymm7, %ymm0, %ymm0
+	vpsllvq	32(%rax), %ymm0, %ymm8
+	vpsrlvq	32(%rcx), %ymm0, %ymm0
+	vpor	%ymm8, %ymm0, %ymm0
+	vpxor	%ymm7, %ymm6, %ymm6
+	vpermq	$-115, %ymm3, %ymm8
+	vpermq	$-115, %ymm4, %ymm9
+	vpsllvq	64(%rax), %ymm6, %ymm3
+	vpsrlvq	64(%rcx), %ymm6, %ymm4
+	vpor	%ymm3, %ymm4, %ymm10
+	vpxor	%ymm7, %ymm2, %ymm2
+	vpermq	$27, %ymm5, %ymm5
+	vpermq	$114, %ymm0, %ymm7
+	vpsllvq	-64(%rax), %ymm2, %ymm0
+	vpsrlvq	-64(%rcx), %ymm2, %ymm2
+	vpor	%ymm0, %ymm2, %ymm2
+	vpsrldq	$8, %ymm10, %ymm0
+	vpandn	%ymm0, %ymm10, %ymm0
+	vpblendd	$12, %ymm7, %ymm2, %ymm3
+	vpblendd	$12, %ymm2, %ymm9, %ymm4
+	vpblendd	$12, %ymm9, %ymm8, %ymm6
+	vpblendd	$12, %ymm8, %ymm2, %ymm11
+	vpblendd	$48, %ymm9, %ymm3, %ymm3
+	vpblendd	$48, %ymm5, %ymm4, %ymm4
+	vpblendd	$48, %ymm2, %ymm6, %ymm6
+	vpblendd	$48, %ymm7, %ymm11, %ymm11
+	vpblendd	$-64, %ymm5, %ymm3, %ymm3
+	vpblendd	$-64, %ymm7, %ymm4, %ymm4
+	vpblendd	$-64, %ymm7, %ymm6, %ymm6
+	vpblendd	$-64, %ymm9, %ymm11, %ymm11
+	vpandn	%ymm4, %ymm3, %ymm3
+	vpandn	%ymm11, %ymm6, %ymm4
+	vpblendd	$12, %ymm2, %ymm5, %ymm6
+	vpblendd	$12, %ymm5, %ymm8, %ymm11
+	vpxor	%ymm8, %ymm3, %ymm12
+	vpblendd	$48, %ymm8, %ymm6, %ymm3
+	vpblendd	$48, %ymm9, %ymm11, %ymm6
+	vpxor	%ymm5, %ymm4, %ymm11
+	vpblendd	$-64, %ymm9, %ymm3, %ymm3
+	vpblendd	$-64, %ymm2, %ymm6, %ymm4
+	vpandn	%ymm4, %ymm3, %ymm3
+	vpxor	%ymm7, %ymm3, %ymm6
+	vpermq	$30, %ymm10, %ymm3
+	vpblendd	$48, %ymm1, %ymm3, %ymm3
+	vpermq	$57, %ymm10, %ymm4
+	vpblendd	$-64, %ymm1, %ymm4, %ymm4
+	vpandn	%ymm3, %ymm4, %ymm13
+	vpblendd	$12, %ymm5, %ymm9, %ymm3
+	vpblendd	$12, %ymm9, %ymm7, %ymm4
+	vpblendd	$48, %ymm7, %ymm3, %ymm3
+	vpblendd	$48, %ymm8, %ymm4, %ymm4
+	vpblendd	$-64, %ymm8, %ymm3, %ymm3
+	vpblendd	$-64, %ymm5, %ymm4, %ymm4
+	vpandn	%ymm4, %ymm3, %ymm3
+	vpxor	%ymm2, %ymm3, %ymm3
+	vpermq	$0, %ymm0, %ymm14
+	vpermq	$27, %ymm12, %ymm4
+	vpermq	$-115, %ymm11, %ymm0
+	vpermq	$114, %ymm6, %ymm6
+	vpblendd	$12, %ymm8, %ymm7, %ymm11
+	vpblendd	$12, %ymm7, %ymm5, %ymm7
+	vpblendd	$48, %ymm5, %ymm11, %ymm5
+	vpblendd	$48, %ymm2, %ymm7, %ymm7
+	vpblendd	$-64, %ymm2, %ymm5, %ymm2
+	vpblendd	$-64, %ymm8, %ymm7, %ymm5
+	vpandn	%ymm5, %ymm2, %ymm5
+	vpxor	%ymm14, %ymm1, %ymm1
+	vpxor	%ymm10, %ymm13, %ymm2
+	vpxor	%ymm9, %ymm5, %ymm5
+	vpxor	(%rbp), %ymm1, %ymm1
+	leaq	32(%rbp), %rbp
+	decl	%edx
+	jne 	Lkeccak_1600$5
+	vmovdqu	%ymm1, (%rsp)
+	vmovdqu	%ymm2, 32(%rsp)
+	vmovdqu	%ymm3, 64(%rsp)
+	vmovdqu	%ymm4, 96(%rsp)
+	vmovdqu	%ymm5, 128(%rsp)
+	vmovdqu	%ymm0, 160(%rsp)
+	vmovdqu	%ymm6, 192(%rsp)
+	movq	%rsi, %rax
+	shrq	$3, %rax
+	movq	$0, %rcx
+	jmp 	Lkeccak_1600$3
+Lkeccak_1600$4:
+	movq	(%r9,%rcx,8), %rdx
+	movq	(%rsp,%rdx,8), %rdx
+	movq	%rdx, (%rdi,%rcx,8)
+	leaq	1(%rcx), %rcx
+Lkeccak_1600$3:
+	cmpq	%rax, %rcx
+	jb  	Lkeccak_1600$4
+	movq	(%r9,%rcx,8), %rax
+	shlq	$3, %rcx
+	shlq	$3, %rax
+	jmp 	Lkeccak_1600$1
+Lkeccak_1600$2:
+	movb	(%rsp,%rax), %dl
+	movb	%dl, (%rdi,%rcx)
+	leaq	1(%rcx), %rcx
+	leaq	1(%rax), %rax
+Lkeccak_1600$1:
+	cmpq	%rsi, %rcx
+	jb  	Lkeccak_1600$2
 	addq	$224, %rsp
+	popq	%r14
+	popq	%r13
+	popq	%r12
 	popq	%rbx
 	popq	%rbp
 	ret 
